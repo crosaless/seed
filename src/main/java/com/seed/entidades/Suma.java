5c0dcdc2a67ad9e9b72402107786ffeeab85fafb
cristian <cristiannrosales12@gmail.com>
2024-06-10 11:36:48 -0300

    Initial commit

---
+++ src/main/java/com/seed/entidades/Suma.java
@@ -0,0 +1,8 @@
+package com.seed.entidades;
+
+public class Suma {
+
+    public int Suma(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/com/seed/entidades/SumaTest.java
@@ -0,0 +1,13 @@
+package com.seed.entidades;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SumaTest {
+    @Test
+    public void testSuma(){
+        Suma suma = new Suma();
+        assertEquals(105, suma.Suma(100,5));
+    }
+}
