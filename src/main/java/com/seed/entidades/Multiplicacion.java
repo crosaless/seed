1b45624e856a0679ab9038775c04629c25677aa3
Cristian Rosales <ramon95.reta@hotmail.com>
2024-09-12 10:13:29 -0300

    arreglo suma,multiplicaion,porcentaje y evaluador

--- src/main/java/com/seed/entidades/Multiplicacion.java
+++ src/main/java/com/seed/entidades/Multiplicacion.java
@@ -1,9 +1,20 @@
 
 package com.seed.entidades;
 
-public class Multiplicacion {
-    public double multiplicar(double a, double b){
-        return a*b;
-    }
+/**
+ * Clase que realiza la operación de multiplicación de dos números de tipo doble.
+ */
+public final class Multiplicacion {
 
+    /**
+     * Multiplica dos números de tipo doble.
+     *
+     * @param a el primer número de tipo doble.
+     * @param b el segundo número de tipo doble.
+     * @return el producto de los dos números.
+     */
+    public double multiplicar(final double a, final double b) {
+        return a * b;
+    }
 }
+
