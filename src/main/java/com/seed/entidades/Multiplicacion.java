6aaaee23921cb36b5b43389692ea397f32fb72f5
alear12373 <alear12373@gmail.com>
2024-09-12 11:46:52 -0300

    arreglo 10

--- src/main/java/com/seed/entidades/Multiplicacion.java
+++ src/main/java/com/seed/entidades/Multiplicacion.java
@@ -2,7 +2,7 @@
 package com.seed.entidades;
 
 /**
- * Clase que realiza la operación 
+ * Clase que realiza la operación
  * de multiplicación de dos números de tipo doble.
  */
 public final class Multiplicacion {
@@ -14,7 +14,7 @@ public final class Multiplicacion {
      *el primer número de tipo doble.
      * @param b
      * el segundo número de tipo doble.
-     * 
+     *
      * @return el producto de los dos números.
      */
     public double multiplicar(final double a, final double b) {
--- src/main/java/com/seed/entidades/Resta.java
+++ src/main/java/com/seed/entidades/Resta.java
@@ -12,13 +12,13 @@ public final class Resta {
      * @param minuendo el primer número entero (minuendo).
      * @param sustraendo el segundo número entero (sustraendo).
      * @return el resultado de la resta.
-     * @throws IllegalArgumentException si 
+     * @throws IllegalArgumentException si
      * el minuendo es menor que el sustraendo.
      */
     public int restar(final int minuendo, final int sustraendo) {
         if (minuendo < sustraendo) {
-            throw new IllegalArgumentException
-            ("El minuendo no puede ser menor que el sustraendo.");
+            throw new IllegalArgumentException(
+                "El minuendo no puede ser menor que el sustraendo.");
         }
         return minuendo - sustraendo;
     }
