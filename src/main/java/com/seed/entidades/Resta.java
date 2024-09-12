1b0437a92c97b6cf5ac39725f7ea15c0348597f6
alear12373 <alear12373@gmail.com>
2024-09-12 10:37:58 -0300

    arreglar division y resta

--- src/main/java/com/seed/entidades/Resta.java
+++ src/main/java/com/seed/entidades/Resta.java
@@ -1,7 +1,18 @@
 package com.seed.entidades;
 
+/**
+ * Clase que realiza la operación de resta de dos números enteros.
+ */
 public class Resta {
-    public int restar(int a, int b){
-        return a-b;
+
+    /**
+     * Resta dos números enteros.
+     *
+     * @param a el minuendo.
+     * @param b el sustraendo.
+     * @return el resultado de la resta.
+     */
+    public int restar(final int a, final int b) {
+        return a - b;
     }
 }
