72e99edde6d4eee9a5bdeda248af25343804c8fe
alear12373 <alear12373@gmail.com>
2024-09-12 10:04:12 -0300

    arreglo producto

--- src/main/java/com/seed/entidades/Producto.java
+++ src/main/java/com/seed/entidades/Producto.java
@@ -1,17 +1,60 @@
 package com.seed.entidades;
 
-public class Producto {
-    public int stock = 60;
+/**
+ * Clase que representa un producto.
+ */
+final class Producto {
+
+    /**
+     * Stock del producto.
+     */
+    private int stock = INITIAL_STOCK;
+
+    /**
+     * Nombre del producto.
+     */
     private String nombre;
-    public int disminuir(int a){
-        return stock= stock - a;
+
+    /**
+     * Stock inicial.
+     */
+    private static final int INITIAL_STOCK = 60;
+
+    /**
+     * Disminuye el stock del producto.
+     * 
+     * @param cantidad Cantidad a disminuir.
+     * @return Stock actualizado.
+     */
+    public int disminuir(final int cantidad) {
+        stock = stock - cantidad;
+        return stock;
     }
 
-    public void setNombre(String nombre){
-        this.nombre=nombre;
+    /**
+     * Establece el nombre del producto.
+     * 
+     * @param nombre Nombre del producto.
+     */
+    public void setNombre(final String nombre) {
+        this.nombre = nombre;
     }
-    
-    public String getNombre(){
+
+    /**
+     * Obtiene el nombre del producto.
+     * 
+     * @return El nombre del producto.
+     */
+    public String getNombre() {
         return nombre;
+    }
+
+    /**
+     * Obtiene el stock actual del producto.
+     * 
+     * @return El stock actual.
+     */
+    public int getStock() {
+        return stock;
     }
 }
