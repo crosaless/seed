a370946683122337c62c91216a9315993d7c4d91
Cristian Rosales <ramon95.reta@hotmail.com>
2024-09-12 00:50:02 -0300

    prueba3

--- src/main/java/com/seed/entidades/Potencia.java
+++ src/main/java/com/seed/entidades/Potencia.java
@@ -2,7 +2,7 @@ package com.seed.entidades;
 
 
 public class Potencia {
-
+    int nro1;
     public int potencia(int nro, int pot) {
         int resultado = 1;
         for (int i = 0; i < pot; i++) {
